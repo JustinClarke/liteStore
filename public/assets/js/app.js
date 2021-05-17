17f3e683b38cd9bfde63ce7abbcf26d1e7d43c62
Justin Clarke <justinclarke241@gmail.com>
2021-05-17 15:17:51 +0530

    added app.js

---
+++ public/assets/js/app.js
@@ -0,0 +1,26 @@
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+var firebaseConfig = {
+    authDomain: "litestore-in.firebaseapp.com",
+    databaseURL: "https://litestore-in.firebaseio.com",
+    projectId: "litestore-in",
+    storageBucket: "litestore-in.appspot.com",
+    appId: "1:493025178485:web:b20223af21733687b3193c",
+    measurementId: "G-MEASUREMENT_ID",
+  };
+  firebase.initializeApp(config);
+  
+  //Get elements
+  const inputEmail = document.getElementById('inputEmail')
+  const inputPassword = document.getElementById('inputPassword')
+  const loginButton = document.getElementById('loginButton')
+  const logoutButton = document.getElementById('logoutButton')
+  
+  //add login event
+  loginButton.addEventListener('click', e =>{
+    const email = inputEmail.value;
+    const pass = inputPassword.value;
+    const auth = firebase.auth();
+    //signin
+    const promise = auth.signInWithEmailAndPassword(email, pass)
+    promise.catch(e => console.log(e.message))
+  });  
